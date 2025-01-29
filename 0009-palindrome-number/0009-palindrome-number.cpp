cbac643a991a06c6e1e4a3b0e8e175c5498f6f3a
Vikash Kumar <rajvikash18113@gmail.com>
2025-01-29 11:54:41 +0530

    Time: 0 ms (100%), Space: 8.6 MB (42.41%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,7 +3,7 @@ public:
     bool isPalindrome(int x) {
 
         int n = x;
-
+        // if the number is negative or less than 0 then return false
         if (n < 0)
             return false;
 
@@ -11,7 +11,8 @@ public:
 
         while (n > 0) {
             int digit = n % 10;
-
+            // check for integer OverFlow and UnderFlow
+            // If the output is greater or smaller than the range of integer value then return false
             if (reverse > INT_MAX / 10 || reverse < INT_MIN / 10)
                 return false;
 
@@ -19,9 +20,6 @@ public:
             n /= 10;
         }
 
-        if (reverse == x)
-            return true;
-        else
-            return false;
+        return (reverse == x);
     }
 };
