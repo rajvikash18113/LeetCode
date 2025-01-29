ac794277bbcc121b1e3d069b35601b2a021ee510
Vikash Kumar <rajvikash18113@gmail.com>
2025-01-29 11:55:17 +0530

    Time: 5 ms (22.32%), Space: 8.5 MB (68.68%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -12,14 +12,14 @@ public:
         while (n > 0) {
             int digit = n % 10;
             // check for integer OverFlow and UnderFlow
-            // If the output is greater or smaller than the range of integer value then return false
+            // If the output is greater or smaller than the range of integer
+            // value then return false
             if (reverse > INT_MAX / 10 || reverse < INT_MIN / 10)
                 return false;
 
             reverse = reverse * 10 + digit;
             n /= 10;
         }
-
         return (reverse == x);
     }
 };
