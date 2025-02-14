2e1ac14e17b81ebb9d0ed5e93be65b692c61d9f1
Vikash Kumar <rajvikash18113@gmail.com>
2025-02-14 11:32:49 +0530

    Time: 0 ms (100%), Space: 21.9 MB (14.51%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int arrSum = 0;
-        int sum = 0;
         int n = nums.size();
-        for (int i = 1; i <= n; i++) {
-            sum += i;
-            arrSum += nums[i - 1];
-        }
+        int sum = (n * (n + 1)) / 2;
+        
+        // accumulate function is a part of <numeric> library in C++ which is
+        // used to  calculate the sum of the given set.
+        int arrSum = accumulate(nums.begin(), nums.end(),0); 
+        // 0 means initial sum of the set is 0
+
         return sum - arrSum;
     }
 };
