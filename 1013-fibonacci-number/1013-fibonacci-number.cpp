4bb6e054352e938ef91ba05ecde5e76766702188
Vikash Kumar <rajvikash18113@gmail.com>
2025-06-05 11:36:45 +0530

    Time: 13 ms (29.89%), Space: 7.9 MB (22.56%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        // Using Loop
-        int i = 1;
-        int first = 0, second = 1, sum = 0;
-        if(n <= 1) return n;
-        while(i < n)
-        {
-            sum = first + second;
-            first = second;
-            second = sum;
-            i++;
-        }
-        return sum;
+        if (n <= 1)
+            return n;
+        // multiple recursion call
+        return fib(n - 1) + fib(n - 2);
     }
 };
