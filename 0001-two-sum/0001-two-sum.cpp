b372ec251949f349dd4557f22ba09c235dbacc24
Vikash Kumar <rajvikash18113@gmail.com>
2025-06-18 15:23:52 +0530

    Time: 2 ms (76.4%), Space: 14.9 MB (21.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
+//---------------- Beter Approach : HashMap---------------//
     vector<int> twoSum(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
-        vector<pair<int, int>> arr;
+        unordered_map<int, int> mpp;
         for (int i = 0; i < nums.size(); i++) {
-            arr.push_back({nums[i], i});
-        }
-        sort(arr.begin(), arr.end());
-
-        while (left < right) {
-            int sum = arr[left].first + arr[right].first;
-            if (sum < target) {
-                left++;
-            } else if (sum > target) {
-                right--;
-            } else {
-                return {arr[left].second, arr[right].second};
+            if (mpp.find(target - nums[i]) != mpp.end()) {
+                return {i, mpp[target - nums[i]]};
+            }
+            if (mpp.find(target - nums[i]) == mpp.end()) {
+                mpp[nums[i]] = i;
             }
         }
         return {};
