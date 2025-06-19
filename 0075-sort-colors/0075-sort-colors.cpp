ba5c518ec99e7f6c300e81deac805d403858abf9
Vikash Kumar <rajvikash18113@gmail.com>
2025-06-19 23:08:43 +0530

    Time: 0 ms (100%), Space: 11.7 MB (12.61%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,42 +1,21 @@
 class Solution {
 public:
+    //-------Optimized Approach : Dutch National Flag Problem-------//
     void sortColors(vector<int>& nums) {
-        int c0 = 0;
-        int c1 = 0;
-        int c2 = 0;
-        int n = nums.size();
-        //vector<int>arr;
-
-        for(int i = 0; i < n; i++)
-        {
-            if(nums[i] == 0)
-                c0++;
-            else if(nums[i] == 1)
-                c1++;
-            else if(nums[i] == 2)
-                c2++;
+        int left = 0;
+        int mid = 0;
+        int right = nums.size() - 1;
+        while (mid <= right) {
+            if (nums[mid] == 0) {
+                swap(nums[left], nums[mid]);
+                left++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[mid], nums[right]);
+                right--;
+            }
         }
-        
-        int index = 0;
-            for(int i = 0; i < c0; i++)
-            {
-                nums[index++] = 0;
-            }
-
-            for(int i = 0; i < c1; i++)
-            {
-                nums[index++] = 1;
-            }
-
-            for(int i = 0; i < c2; i++)
-            {
-                nums[index++] = 2;
-            }
-
-            for(int i = 0; i < n; i++)
-            {
-                cout << nums[i] << " ";
-            }
-            cout << endl;
     }
 };
