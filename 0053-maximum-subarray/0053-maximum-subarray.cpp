9ce2dd6d26e3e6ef187409e88ee02cae38de8a8f
Vikash Kumar <rajvikash18113@gmail.com>
2025-06-22 11:49:05 +0530

    Time: 0 ms (100%), Space: 71.6 MB (80.74%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
+    //----------Optimal Approach : Kadane's Algorithm -----------//
     int maxSubArray(vector<int>& nums) {
         int sum = nums[0];
         int maxi = nums[0];
-        for(int i = 1; i < nums.size(); i++){
+        for (int i = 1; i < nums.size(); i++) {
             sum = max(nums[i], sum + nums[i]);
             maxi = max(maxi, sum);
         }
