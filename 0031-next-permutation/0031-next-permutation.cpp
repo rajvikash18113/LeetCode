93b00bf54cdf7bdfc70f2d146a5164a9c91d3f77
Vikash Kumar <rajvikash18113@gmail.com>
2025-06-23 17:53:23 +0530

    Time: 0 ms (100%), Space: 15.6 MB (92.24%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,26 @@
 class Solution {
 public:
-    //------------Using Built-in Function----------------//
+    //--------Expand of Inbuilt C++ STL function to find Next
+    //Permutation.--------//
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int n = nums.size();
+        int index = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        for (int i = n - 1; i > index; i--) {
+            if (nums[i] > nums[index]) {
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+        reverse(nums.begin() + index + 1, nums.end());
     }
 };
