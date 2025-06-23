305e5e9afa4b70092b5f8e362351ffa1ca38c288
Vikash Kumar <rajvikash18113@gmail.com>
2025-06-23 12:27:48 +0530

    Time: 0 ms (100%), Space: 15.9 MB (32.9%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    //------------Using Built-in Function----------------//
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(), nums.end());
+    }
+};
