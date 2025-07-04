544795d25dc16b8773550aad8b3c60f036d0bb8d
Vikash Kumar <rajvikash18113@gmail.com>
2025-07-04 17:39:55 +0530

    Time: 0 ms (100%), Space: 23.6 MB (57.57%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,17 +1,41 @@
 class Solution {
 public:
-    //------------Better Approach------------//
+    //--------Optimal Approach : Moore's voting Algorithm--------//
     vector<int> majorityElement(vector<int>& nums) {
+        int count1 = 0, count2 = 0;
+        int element1 = INT_MIN;
+        int element2 = INT_MIN;
         int n = nums.size();
-        map<int, int> mpp;
         vector<int> ans;
         for (int i = 0; i < n; i++) {
-            mpp[nums[i]]++;
-        }
-        for (auto i : mpp) {
-            if (i.second > (n / 3)) {
-                ans.push_back(i.first);
+            if (count1 == 0 && element2 != nums[i]) {
+                element1 = nums[i];
+                count1++;
+            } else if (count2 == 0 && element1 != nums[i]) {
+                element2 = nums[i];
+                count2++;
+            } else if (nums[i] == element1) {
+                count1++;
+            } else if (nums[i] == element2) {
+                count2++;
+            } else {
+                count1--;
+                count2--;
             }
+        }
+        int count3 = 0, count4 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == element1) {
+                count3++;
+            } else if (nums[i] == element2) {
+                count4++;
+            }
+        }
+        if (count3 > (n / 3)) {
+            ans.push_back(element1);
+        }
+        if (count4 > (n / 3)) {
+            ans.push_back(element2);
         }
         return ans;
     }
