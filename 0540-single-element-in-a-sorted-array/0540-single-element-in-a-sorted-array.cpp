ba78558a3c800346ef5480f618fd14e33121237c
Vikash Kumar <rajvikash18113@gmail.com>
2025-07-19 06:34:30 +0530

    Time: 0 ms (100%), Space: 26.3 MB (7.27%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -13,13 +13,16 @@ public:
         int low = 1, high = n - 2;
         while (low <= high) {
             int mid = (low + high) / 2;
-            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+            if (nums[mid] != nums[mid - 1] &&
+                nums[mid] != nums[mid + 1]) { // Element Found
                 return nums[mid];
             }
+            // We are in left half and answer is in right half
             if (mid % 2 == 0 && nums[mid] == nums[mid + 1] ||
                 mid % 2 == 1 && nums[mid] == nums[mid - 1]) {
                 low = mid + 1;
             } else {
+                // We are in right half and answer is in left half
                 high = mid - 1;
             }
         }
